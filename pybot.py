a409fb8c2cecd15b8c8359ee0a69a61d134f5dba
thunderd568 <nationals07@msn.com>
2019-07-22 21:45:54 -0400

    Completely redid the image recognition. Now using face_recognition to detect bts images and memes. Still imperfect but seems to do the job for the most part.

--- pybot.py
+++ pybot.py
@@ -1,13 +1,55 @@
+import aiohttp
 import discord
 import time
 import random
 import asyncio
+
+import requests
 from pyjokes import pyjokes # Despite the red lines, this works just fine. no errors
 from discord.ext import commands
 from django.core.validators import URLValidator
 from django.core.exceptions import ValidationError
 from apiclient.discovery import build
+from io import BytesIO
 import urllib.parse as urlparse
+import urllib.request
+import face_recognition
+import os
+from PIL import Image
+
+
+# Constants
+BTS_IMAGES_PATH = './BTS Members/'
+MESSAGE_IMGS_PATH ='./Message Imgs/'
+
+
+def image_is_bts_content(img):
+    tolerance = 0.5 # Threshold of match for the image.
+
+    # Encode the picture and prepare it for facial recognition.
+    picture = face_recognition.load_image_file(img)
+    picture_encoding = face_recognition.face_encodings(picture)[0]  # I just want the first item
+
+    for file in os.listdir(BTS_IMAGES_PATH):
+        # For every file in BTS members, check if it is in fact one of the members.
+        # The bts picture to compare it to.
+        bts_picutre = face_recognition.load_image_file(BTS_IMAGES_PATH + '/' + file)
+        bts_picture_encoding = face_recognition.face_encodings(bts_picutre)[0]
+
+        # Compare faces
+        results = face_recognition.compare_faces([picture_encoding], bts_picture_encoding, tolerance=tolerance)
+        print(f'Results after comparing to {file}: {results}')
+
+        if results[0]:
+            print("Match Found with file " + file)
+            return True
+    #endfor
+
+    print("No match found")
+    return False
+
+#enddef
+
 
 class MyClient(discord.Client):
 
@@ -17,7 +59,8 @@ class MyClient(discord.Client):
         print('------')
         servers = list(client.guilds)
 
-        commands.Bot(command_prefix='.')
+        self.messages_deleted = 0
+        self.messages_edited = 0
 
         # Print the servers the bot is currently connected to.
         for x in range(len(servers)):
@@ -35,11 +78,20 @@ class MyClient(discord.Client):
         #endfor
     #enddef
 
+
+    """Method to display status of Pybot"""
+    async def status(self, message):
+        await message.channel.send('Online. 0 user suspended\n{} messages deleted. {} edited.'.format(self.messages_deleted, self.messages_edited))
+    #enddef
+
+
     async def on_message(self, message):
         # we do not want the bot to reply to itself
         if message.author.id == self.user.id:
             return
         #endif
+
+
 
         # Save the author in a variable.
         author = str(message.author.name)
@@ -47,12 +99,20 @@ class MyClient(discord.Client):
             real_name = real_names[str(author)]
         else:
             real_name = author
+        #endif
+
 
         # Alert of incoming message and specify the author.
         print('Message from ' + real_name + ': ' + str(message.content))
 
+        # If the status command is entered by me and me only.
+        if message.content.startswith('.status') and real_name == ME:
+            time.sleep(2.0)
+            await self.status(message)
+        # endif
+
         # First thing to do is to check if the message contains a URL
-        if (self.is_url_(message)):
+        if (self.is_url_(message) and 'youtube' in str(message.content)) and 'youtu.be' in str(message.content):
             print("URL Detected")
 
             if self.is_bts_video(message):
@@ -61,7 +121,7 @@ class MyClient(discord.Client):
                 time.sleep(2.0)
                 await message.delete()
                 await message.channel.send('Pop!')
-
+                self.messages_deleted = self.messages_deleted + 1
         #endif
 
         if message.content.startswith('hello') or message.content.startswith('Hello') or message.content.startswith('hi') or message.content.startswith('hey'):
@@ -72,8 +132,8 @@ class MyClient(discord.Client):
             await message.channel.send(random_greeting + ' ' + real_name.format(message))
         #endif
 
-        elif message.content.startswith('Good night') or message.content.startswith('good night'):
-            time.sleep(4.0)
+        elif 'say good night pybot' in message.content.lower():
+            time.sleep(3.0)
             await message.channel.send('Good night everyone!')
         #endelif
 
@@ -115,17 +175,23 @@ class MyClient(discord.Client):
             await self.respond_to_khan(message)
         #endelif
 
+        # If the message has files and attachments check if its bts content.
         elif message.attachments:
-            attachments = message.attachments
-            print(attachments)
+            attachments = message.attachments   # Get the list of attachments in the message.
+            print(attachments)  # See the contents of the list.
             filename = attachments[len(attachments) - 1].filename
+            url = attachments[len(attachments) - 1].proxy_url   # Store the filename of the last one sent.
+            print(f'URL of image: {url} loaded')
 
-            if 'bts' in filename.lower() or 'screenshot' in filename.lower():
+            r = requests.get(url)
+            img = BytesIO(r.content)
+
+            if image_is_bts_content(img):
                 time.sleep(2.0)
-                await message.channel.send('Sorry {} you were warned. No BTS photos allowed'.format(real_name))
+                await message.channel.send('BTS detected! No BTS memes or pictures are allowed {}!'.format(real_name))
                 time.sleep(2.0)
                 await message.delete()
-
+            #endif
         #endelif
 
         # If someone wants to thank pybot
@@ -139,8 +205,27 @@ class MyClient(discord.Client):
             await self.respond_to_dakota(message)
         #endelif
 
+        # If the shutdown command is given. End the script.
+        elif client.user in message.mentions and '.shutdown' in str(message.content) and real_name == ME:
+            time.sleep(2.0)
+            await message.channel.send('Pybot logging off...')
+            exit()
+        #endelif
+
 
     #enddef
+
+    def bts_members_in_filename(self, filename):
+        for member in bts_members:
+
+            if member in filename:
+                print('Found {} in filename'.format(member) )
+                return True
+            #endif
+        #endfor
+        return False
+    #endef
+
 
     async def on_member_join(self, member):
         guild = member.guild
@@ -154,6 +239,8 @@ class MyClient(discord.Client):
         if str(before.content) == str(after.content):
             return
         #endif
+
+        self.messages_edited = self.messages_edited + 1
 
         fmt = '**{0.author}** edited their message:\n{0.content} -> {1.content}'
         await before.channel.send(fmt.format(before, after))
@@ -218,6 +305,7 @@ client = MyClient()
 
 #api key for youtube
 API_KEY = 'AIzaSyDDh4akmSBgNntLYpc4gJIe3u8kP5lzZLU'
+ME = 'Phil'
 
 real_names = {'Dread_Lock': 'Dakota',
               'thunderD568': 'Phil',
@@ -225,6 +313,9 @@ real_names = {'Dread_Lock': 'Dakota',
               '(っ◔◡◔)っ lï††lêþåñ¢kê§': 'Kirra',
               'RunningRaptor22':'Grant',
               'カジュアル': 'Khan'}
+
+#List of BTS members
+bts_members = ['jin', 'suga', 'junghook', 'rm', ' v ', 'jimin' ]
 
 #array of greeings for the bot
 greetings = ['Hey',
